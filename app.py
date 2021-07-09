b656f76128b4b6f8dcf8beb447972f10326c3a49
Phạm Quang Vinh <55485505+VinhPhamBG@users.noreply.github.com>
2021-07-09 19:23:04 +0700

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ elif url:
 
 img_test = preprocess_image('./test.jpg')
 
-model = load_model("model/best_model.h5")
+model = load_model("./model/best_model.h5")
 pred_probs = model.predict(img_test)[0]
 print(pred_probs)
 
