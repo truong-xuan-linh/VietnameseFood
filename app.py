4d650bcfdddc409f7d076cd5ce0f0e28c2490f19
Phạm Quang Vinh <55485505+VinhPhamBG@users.noreply.github.com>
2021-07-10 19:52:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ classes = [
 
 with open("food.txt", "r", encoding="utf-8") as f:
      food = ast.literal_eval(f.read())
-st.markdown(food['banh_'])
+st.markdown(food['banh_mi'])
 def preprocess_image(img_path):
     img = image.load_img(img_path, target_size=(300, 300))
     img = image.img_to_array(img) / 255
