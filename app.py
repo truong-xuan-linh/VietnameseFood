d24389da5dde16047998dd611c545a1e452ad7a3
Phạm Quang Vinh <55485505+VinhPhamBG@users.noreply.github.com>
2021-07-10 18:38:00 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,7 @@ from tensorflow.keras.applications.efficientnet import EfficientNetB0, preproces
 from tensorflow.keras.optimizers import *
 from tensorflow.keras.utils import *
 from tensorflow.keras.initializers import *
-"""
-WIDTH = 224
-HEIGHT = 224
-LEARNING_RATE = 0.0001
-CLASSES = 30
-SEED = 120
-"""
+
 classes = [
     'banh_mi_nuong', 
     'com_chay', 
@@ -111,18 +105,6 @@ def main():
 
     img_test = preprocess_image('./test.jpg')
     
-    """
-    base_model = EfficientNetB0(include_top = False, weights = 'imagenet', input_shape = (WIDTH, HEIGHT, 3))
-    x = base_model.output
-    x = GlobalAveragePooling2D()(x)
-    x = BatchNormalization()(x)
-    x = Dense(1280, activation = 'relu', kernel_initializer = glorot_uniform(SEED), bias_initializer = 'zeros')(x)
-    x = BatchNormalization()(x)
-    predictions = Dense(CLASSES, activation = 'softmax', kernel_initializer = 'random_uniform', bias_initializer = 'zeros')(x)
-    model = Model(inputs = base_model.input, outputs = predictions)
-    for layer in model.layers: layer.trainable = True
-    model.compile(optimizer = Adam(lr = LEARNING_RATE), loss = 'categorical_crossentropy', metrics = ['accuracy'])
-    """
     model_path = 'model/final_model.h5'
     model = load_model(model_path)
     #model.load_weights(model_path)
